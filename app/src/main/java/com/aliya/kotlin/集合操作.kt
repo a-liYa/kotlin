ca2c448de43829c3aa25ad500a69e3a83e0f2c02
a_liYa <a_liYa@126.com>
2020-10-23 11:45:43 +0800

    添加 集合过滤操作 示例

--- app/src/main/java/com/aliya/kotlin/集合操作.kt
+++ app/src/main/java/com/aliya/kotlin/集合操作.kt
@@ -56,6 +56,33 @@ private fun filterOpt() {
     println(filterResults === filterTo) // true
     println(filterResults == filterIndexedTo) // true
 
+
+    val (match, rest) = numbers.partition { it.length > 3 }
+    println(match)
+    println(rest)
+
+    // HashMap filter
+    val numbersMap = mapOf("key1" to 1, "key2" to 2, "key3" to 3, "key11" to 11)
+    val filteredMap = numbersMap.filter { (key, value) -> key.endsWith("1") && value > 10 }
+    println(filteredMap)
+
+    val anyNumbers = listOf(null, 1, "two", 3.0, "four")
+    println("All String elements :")
+    println(anyNumbers.filterIsInstance<String>())
+
+    // 返回所有的非空元素
+    println(anyNumbers.filterNotNull())
+
+    // 检验谓词
+
+    // 如果至少有一个元素匹配给定谓词，那么 any() 返回 true
+    println(numbers.any { it.endsWith("e") })
+
+    // 如果没有元素与给定谓词匹配，那么 none() 返回 true
+    println(numbers.none { it.endsWith("a") })
+
+    // 如果所有元素都匹配给定谓词，那么 all() 返回 true
+    println(numbers.all { it.endsWith("e") })
 }
 
 /**
