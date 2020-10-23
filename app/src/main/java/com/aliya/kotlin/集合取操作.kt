8f422ad07bd62e18f46a825650e0c6c47967e648
a_liYa <a_liYa@126.com>
2020-10-23 15:34:52 +0800

    添加 集合取单个元素 示例

--- app/src/main/java/com/aliya/kotlin/集合取操作.kt
+++ app/src/main/java/com/aliya/kotlin/集合取操作.kt
@@ -34,6 +34,18 @@ fun main() {
     printDivider(" zipWith ")
 
     zipWindowOpt()
+
+    printDivider(" elementAt ")
+
+    elementAt()
+
+    printDivider(" firstOrLast ")
+
+    firstOrLast()
+
+    printDivider(" contains ")
+
+    contains()
 }
 
 /**
@@ -101,4 +113,48 @@ private fun zipWindowOpt() {
     val numbers = listOf("one", "two", "three", "four", "five")
     println(numbers.zipWithNext())
     println(numbers.zipWithNext() { s1, s2 -> s1.length > s2.length})
+}
+
+/**
+ * 按位置取，Set集合会用到
+ */
+private fun elementAt() {
+    val numbers = linkedSetOf("one", "two", "three", "four", "five")
+    println(numbers.elementAt(3))
+
+    val numbersSortedSet = sortedSetOf("one", "two", "three", "four")
+    println(numbersSortedSet.elementAt(0)) // 元素以升序存储
+
+    // 避免位置越界异常，可使用 elementAt() 的安全变体
+    println(numbers.elementAtOrNull(5))
+    // 越界时会 返回拖尾函数给定的值
+    println(numbers.elementAtOrElse(5) { index -> "The value for index $index is undefined"})
+}
+
+/**
+ * 按条件取
+ */
+private fun firstOrLast() {
+    val numbers = listOf("one", "two", "three", "four", "five", "six")
+    println(numbers.first { it.length > 3 })
+    println(numbers.last { it.startsWith("f") })
+
+    // 如果没有元素与谓词匹配，两个函数都会抛异常,规避请改用
+    println(numbers.firstOrNull { it.length > 6 })
+    println(numbers.lastOrNull { it.length > 6 })
+    // 等价于 以下两行代码
+    println(numbers.find { it.length > 6 })
+    println(numbers.findLast { it.length > 6 })
+}
+
+/**
+ * 检测存在与否
+ */
+private fun contains() {
+    val numbers = listOf("one", "two", "three", "four", "five", "six")
+    println(numbers.contains("four"))
+    println("zero" in numbers)
+
+    println(numbers.containsAll(listOf("four", "two")))
+    println(numbers.containsAll(listOf("one", "zero")))
 }
