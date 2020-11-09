73e85a2a850693ce98b2e0f5c87ecfb8c1585fee
a_liYa <liyaf@8531.cn>
2020-11-09 23:30:16 +0800

    添加 协程上下文与调度器

---
+++ app/src/main/java/com/aliya/kotlin/协程上下文与调度器.kt
@@ -0,0 +1,138 @@
+package com.aliya.kotlin
+
+import com.aliya.kotlin.util.printDivider
+import kotlinx.coroutines.*
+
+/**
+ * 协程上下文与调度器
+ *
+ * @author a_liYa
+ * @date 2020/11/9 22:55.
+ *
+ */
+fun main() {
+
+    // 调度器与线程
+    runBlocking {
+        launch { // 运行在父协程的上下文中，即 runBlocking 主协程
+            println("main runBlocking      : I'm working in thread ${Thread.currentThread().name}")
+        }
+        launch(Dispatchers.Unconfined) { // 不受限的——将工作在主线程中
+            println("Unconfined            : I'm working in thread ${Thread.currentThread().name}")
+        }
+        launch(Dispatchers.Default) { // 将会获取默认调度器
+            println("Default               : I'm working in thread ${Thread.currentThread().name}")
+        }
+        launch(newSingleThreadContext("MyOwnThread")) { // 将使它获得一个新的线程
+            println("newSingleThreadContext: I'm working in thread ${Thread.currentThread().name}")
+        }
+    }
+
+    // 非受限调度器 vs 受限调度器
+    runBlocking {
+
+        val a = async {
+            log("I'm computing a piece of the answer")
+            6
+        }
+        val b = async {
+            log("I'm computing another piece of the answer")
+            7
+        }
+        log("The answer is ${a.await() * b.await()}")
+    }
+
+    newSingleThreadContext("Ctx1").use { ctx1 ->
+        newSingleThreadContext("Ctx2").use { ctx2 ->
+            runBlocking(ctx1) {
+                log("Started in ctx1")
+                withContext(ctx2) {
+                    log("Working in ctx2")
+                }
+                log("Back to ctx1")
+            }
+        }
+    }
+
+    // 启动一个协程来处理某种传入请求（request）
+    runBlocking {
+        val request = launch {
+            // 孵化了两个子作业, 其中一个通过 GlobalScope 启动
+            GlobalScope.launch {
+                println("job1: I run in GlobalScope and execute independently!")
+                delay(1000)
+                println("job1: I am not affected by cancellation of the request")
+            }
+            // 另一个则承袭了父协程的上下文
+            launch {
+                delay(100)
+                println("job2: I am a child of the request coroutine")
+                delay(1000)
+                println("job2: I will not execute this line if my parent request is cancelled")
+            }
+        }
+        delay(500)
+        request.cancel() // 取消请求（request）的执行
+        delay(1000) // 延迟一秒钟来看看发生了什么
+        println("main: Who has survived request cancellation?")
+    }
+
+    coroutineNameSample()
+
+    printDivider("协程作用域")
+
+    // 协程作用域
+    runBlocking {
+        val activity = Activity()
+        activity.doSomething() // 运行测试函数
+        println("Launched coroutines")
+        delay(500L) // 延迟半秒钟
+        println("Destroying activity!")
+        activity.destroy() // 取消所有的协程
+        delay(1000) // 为了在视觉上确认它们没有工作
+    }
+}
+
+private fun log(msg: String) = println("[${Thread.currentThread().name}] $msg")
+
+
+private fun coroutineNameSample() = runBlocking {
+    log("Started main coroutine")
+    // 运行两个后台值计算
+    val v1 = async(CoroutineName("v1coroutine")) {
+        delay(500)
+        log("Computing v1")
+        252
+    }
+    val v2 = async(CoroutineName("v2coroutine")) {
+        delay(1000)
+        log("Computing v2")
+        6
+    }
+    log("The answer for v1 / v2 = ${v1.await() / v2.await()}")
+
+    launch(Dispatchers.Default + CoroutineName("test")) {
+        println("I'm working in thread ${Thread.currentThread().name}")
+    }
+}
+
+class Activity {
+    // MainScope() Android 环境使用
+    private val mainScope = CoroutineScope(Dispatchers.Default) // use Default for test purposes
+
+    fun destroy() {
+        mainScope.cancel()
+    }
+
+    // 在 Activity 类中
+    fun doSomething() {
+        // 在示例中启动了 10 个协程，且每个都工作了不同的时长
+        repeat(10) { i ->
+            mainScope.launch {
+                delay((i + 1) * 200L) // 延迟 200 毫秒、400 毫秒、600 毫秒等等不同的时间
+                println("Coroutine $i is done")
+            }
+        }
+    }
+    // Activity 类结束
+}
